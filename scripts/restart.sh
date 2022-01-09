55e3ee9cf856cbaa616b6438c6484aebe60cc09a
Mehmet Zahit GÜNEY <mehmet.guney@turkcell.com.tr>
2022-01-09 15:16:23 +0300

    sh scripts is updated

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -2,7 +2,7 @@ cd ../
 
 killall -9 java
 
-nohup java -Xmx512m -Xms512m -Dexecutor.corePoolSize=100 -Duser.timezone="Europe/Istanbul" -Dlogging.level.com.momoli=debug -jar kripto-arbitraj-0.0.1-SNAPSHOT.jar &
+nohup java -Xmx512m -Xms512m -Duser.timezone="Europe/Istanbul" -Dserver.port=5353 -jar crypto-exchange-price-action-0.0.1-SNAPSHOT.jar &
 
 echo disown
 
--- scripts/start.sh
+++ scripts/start.sh
@@ -11,7 +11,7 @@ cd ../
 
 killall -9  java
 
-nohup java -Xmx512m -Xms512m -Duser.timezone="Europe/Istanbul" -jar crypto-exchange-price-action-0.0.1-SNAPSHOT.jar &
+nohup java -Xmx512m -Xms512m -Duser.timezone="Europe/Istanbul" -Dserver.port=5353 -jar crypto-exchange-price-action-0.0.1-SNAPSHOT.jar &
 
 echo disown
 
