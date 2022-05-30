98524cc26b1275f157d7d8c9686e69586d76a0eb
Mehmet Zahit Güney <mehmetzahitt@gmail.com>
2022-05-30 17:27:04 +0300

    run.sh is changed

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,7 +4,6 @@ nohup java -Dcom.sun.management.jmxremote=true \
 -Dcom.sun.management.jmxremote.port=9050 \
 -Dcom.sun.management.jmxremote.rmi.port=9050 \
 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false \
--Dexecutor.corePoolSize=100 \
 -Xmx512m -Xms512m -Duser.timezone="Europe/Istanbul" \
 -jar crypto-exchange-price-action-0.0.1-SNAPSHOT.jar > nohup.out 2>&1 & sleep 5 & disown &
 
